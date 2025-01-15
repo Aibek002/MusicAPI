58c5cc94c7434d5f4ceceb1b70dfdf269380ebc0
Aibek002 <200103346@stu.sdu.edu.kz>
2025-01-15 15:26:51 +0500

    Implement OIDC authentication with Keycloak

--- components/AuthHandler.php
+++ components/AuthHandler.php
@@ -21,6 +21,7 @@ class AuthHandler
         $email = $userAttributes['email'] ?? null;
         $name = $userAttributes['given_name'] ?? null;
         $surname = $userAttributes['family_name'] ?? null;
+        $username = $userAttributes['preferred_username'] ?? null;
 
 
         if ($email === null) {
@@ -28,12 +29,7 @@ class AuthHandler
         }
 
         $user = User::find()->where(['email' => $email])->one();
-        // $auth = Yii::$app->authManager->getRoles();
-        $rolesFromKeycloak = $userAttributes['roles'];
-        
-
         if ($user) {
-
             Yii::$app->user->login($user);
 
         } else {
@@ -41,43 +37,19 @@ class AuthHandler
             $user = new User([
                 'email' => $email,
                 'name' => $name,
-                'surname' => $surname
+                'surname' => $surname,
+                'username' => $username
             ]);
 
 
             if ($user->save()) {
-                $roles_array = []; // Инициализация массива
-
-                // Добавляем роли в массив
-                foreach ($rolesFromKeycloak as $roleName) {
-                    $role = Yii::$app->authManager->getRole($roleName); // Получаем роль по имени
-                
-                    if ($role) {
-                        $roles_array[] = $role->name; // Добавляем имя роли в массив
-                    }
-                }
-                
-                // Проходим по массиву и выполняем действия с каждой ролью
-                foreach ($roles_array as $role_name) {
-                    $getSingleRole = Yii::$app->authManager->getRole($role_name);
-                    $id_user = $user->getId();
-
-                    if ($getSingleRole) {
-                        if (!Yii::$app->authManager->checkAccess($id_user, $role)) {
-                            Yii::$app->authManager->assign($getSingleRole, $id_user);
-                        }
-
-                    }
-                }
-
                 Yii::$app->user->login($user);
-
             } else {
                 throw new \Exception('Не удалось создать нового пользователя');
             }
 
         }
-        return $user;
+        return $userAttributes;
 
     }
 
