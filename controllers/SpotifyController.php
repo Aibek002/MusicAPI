15a915c8f1f9e346e4410f7a3b3b250a0e0ed314
Aibek002 <200103346@stu.sdu.edu.kz>
2025-02-06 17:56:20 +0500

    modified:   controllers/SpotifyController.php 	modified:   views/spotify/index.php

--- controllers/SpotifyController.php
+++ controllers/SpotifyController.php
@@ -39,29 +39,32 @@ class SpotifyController extends Controller
     public function actionIndex()
     {
         $ch = curl_init();
-        $url = "https://api.spotify.com/v1/browse/new-releases?limit=10";
-        $header = Yii::$app->request->headers->get('Authorization');
+        // $url = "https://api.spotify.com/v1/browse/new-releases?offset=0&limit=20";
+        $url = "https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n";
+        $header = 'Authorization:' . Yii::$app->request->headers->get('Authorization');
         $headers = [
-            'Authorization' => 'Bearer BQBKx8PT2GOz69kAickCjydzDqjf-cgW0ha7ZBh5nehCWLk-7qITE3j11LW8gT5ygS1VxiAPYu2F3VrBgSOMiBG3lIsaZgOuieEVj5SV7t3oL1oLHkiHciTmquvLaToaSsBARIK3oogbFBuWXGRHNRsh1dJv_1jbEXzFHf-SkwKPTnEuyb48hiTZsmg_PuMCDmiy1T1aoFTHkf_WSyb-WTbQfDAhvTDW3kNJhAzdVfGSx7GsNJHU-VKYK7CQtlZ9',
+            'Authorization' => $header,
             'Content-Type' => 'application/json',
         ];
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
         $response = curl_exec($ch);
         curl_close($ch);
-        if ($response == false) {
+        if (!$response) {
             $error = curl_error($ch);
             var_dump($error);
             die;
-        } else {
-
-            $data = Json::decode($response);
         }
 
-        return $this->render('index');
+        $data = Json::decode($response);
+        // var_dump($data);
+        // die;
+
+
+        return $this->render('index', ['playlist' => $data]);
     }
     public function actionLogin()
     {
